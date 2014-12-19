304bd686805987b711f0b2d3eff20a2334681439
Simonas Kazlauskas <git@kazlauskas.me>
2014-12-19 22:05:58 +0200

    Add license text

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,25 @@
+// Copyright © 2014, Simonas Kazlauskas <git@kazlauskas.me>
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 //! An implementation of random number generator based on `rdrand` instruction.
 //!
-//! `rdrand` is claimed to be a cryptographically secure PRNG. It is much
-//! faster than `OsRng` (and slower than `StdRng`), but is only supported on
-//! more recent Intel processors.
+//! `rdrand` is claimed to be a cryptographically secure PRNG. It is much faster than `OsRng` (and
+//! slower than `StdRng`), but is only supported on more recent Intel processors.
 //!
 //! The generator provided by this crate is a viable replacement to any
-//! [std::rand](http://doc.rust-lang.org/std/rand/index.html) generator,
-//! however, since nobody has audited Intel hardware yet, the usual disclaimers
-//! apply.
+//! [std::rand](http://doc.rust-lang.org/std/rand/index.html) generator, however, since nobody has
+//! audited Intel hardware yet, the usual disclaimers apply.
 
 #![feature(asm)]
 use std::rand::Rng;
@@ -66,8 +78,8 @@ fn is_supported() -> bool {
 
 
 impl RdRand {
-    /// Build a generator object. The function will only succeed if `rdrand`
-    /// instruction can be successfully used.
+    /// Build a generator object. The function will only succeed if `rdrand` instruction can be
+    /// successfully used.
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     pub fn new() -> Result<RdRand, Error> {
         if is_supported() {
