365728b149d78d6ecc8e098d6fbb0d485d669641
Simonas Kazlauskas <git@kazlauskas.me>
2017-03-11 17:04:35 +0200

    Release 0.1.6

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright © 2014, Simonas Kazlauskas <git@kazlauskas.me>
+// Copyright © 2014, Simonas Kazlauskas <rdrand@kazlauskas.me>
 //
 // Permission to use, copy, modify, and/or distribute this software for any purpose with or without
 // fee is hereby granted, provided that the above copyright notice and this permission notice
